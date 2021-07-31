c15d82546aa7fc0050409bc369764668d7c4d5be
Nghiep Nguyen <nguyenlapnghiep44@gmail.com>
2021-07-31 17:38:53 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection} from './snake.js'
+import {update as updateFood, draw as drawFood} from './food.js'
+import {outsideGrid} from './grid.js'
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.querySelector('#game-board')
+
+function main (currentTime) {
+    if (gameOver) {
+        if (confirm('You lose! Press ok to restart.')){
+            window.location = './'
+        }
+        return 
+    }
+
+    let secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    window.requestAnimationFrame(main)
+
+    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return 
+    
+    lastRenderTime = currentTime
+
+    update()
+    draw() 
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+    updateFood()
+    checkDeath()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+   gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
+
+
+
